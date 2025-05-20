ebfc3f7cc562d49b9f2b8ca3a407c9e2ddb446c7
Ayub Oketch <ayuboketch84@gmail.com>
2025-05-20 14:43:21 +0000

    components changed to herosection for easy identification

--- client/src/components/heroSection.jsx
+++ client/src/components/heroSection.jsx
@@ -2,18 +2,20 @@ import { Button } from "@relume_io/relume-ui";
 import type { ButtonProps } from "@relume_io/relume-ui";
 
 type ImageProps = {
-    src: string;
-    alt?: string;
-  };
-  
-  type Props = {
-    heading: string;
-    description: string;
-    buttons: ButtonProps[];
-    image: ImageProps;
-  };
+  src: string;
+  alt?: string;
+};
 
-export default heroSection = () => {
+type Props = {
+  heading: string;
+  description: string;
+  buttons: ButtonProps[];
+  image: ImageProps;
+};
+
+export type HeroSectionProps = React.ComponentPropsWithoutRef<"section"> & Partial<Props>;
+
+export const HeroSection = () => {
     return (
         <div>
             <h1>Hero Section</h1>
